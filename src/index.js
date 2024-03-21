820b205e0551b79b9ae9041b6b77c1201ca3c9eb
ter-gav <86326060+ter-gav@users.noreply.github.com>
2024-03-21 14:39:47 +0100

    added basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#generated-poem", {
+    strings: "this is a test poem",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
