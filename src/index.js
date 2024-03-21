13217e00ee1fde7dfe3a26455b90f875fc088151
ter-gav <86326060+ter-gav@users.noreply.github.com>
2024-03-21 15:11:17 +0100

    added axios and the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showPoem(response) {
   new Typewriter("#generated-poem", {
-    strings: "this is a test poem",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 20,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let promptInput = document.querySelector("#prompt");
+
+  let apiKey = "dab4o47cf43b881e15a2t0c248003831";
+  let prompt = `Write a poem about ${promptInput.value}`;
+  let context =
+    "You are a poet and can write poems about anything. Keep them short and be polite. Give them a title and return them in a basic HTML structure. For example: <h3>title</h3><p>first line<br />second line<br />third line</p> and so on";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
